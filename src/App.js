0f833e2da657a72b0395d3be42839aec74085ae5
kenessajr <jeanremy1806@gmail.com>
2020-05-04 16:30:07 +0200

    change welcome message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello Kenessa!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
