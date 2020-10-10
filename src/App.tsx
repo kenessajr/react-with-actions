b116b27e7fb92677239c738dfdf83642ac2c1d75
Mateusz Popielarz <pixellos@users.noreply.github.com>
2020-10-10 19:19:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React !!
         </a>
       </header>
     </div>
